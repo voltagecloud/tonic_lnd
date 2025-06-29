740c66abb001c4a19626f30224c504bbce29f3fd
w3irdrobot <rob@w3ird.tech>
2025-06-29 18:58:22 -0400

    refactor: move protos to own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
-#![allow(clippy::large_enum_variant)]
-#![allow(clippy::doc_lazy_continuation)]
-/// This is part of public interface so it's re-exported.
-pub extern crate tonic;
-
 mod client;
 mod error;
+mod protos;
 
 pub use client::*;
 pub use error::ConnectError;
-
-/// [`tonic::Status`] is re-exported as `Error` for convenience.
-pub type Error = tonic::Status;
+pub use protos::*;
